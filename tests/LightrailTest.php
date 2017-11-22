de912297495aebce7d7bbc6c2a3b1368ec9367f5
Tana Jukes <tana.j@lightrail.com>
2017-11-22 11:40:16 -0800

    Whitespace & autoalignment

--- tests/LightrailTest.php
+++ tests/LightrailTest.php
@@ -4,25 +4,22 @@ namespace Lightrail;
 
 require_once __DIR__ . '/../init.php';
 
-$dotenv = new \Dotenv\Dotenv(__DIR__ . "/..");
+$dotenv = new \Dotenv\Dotenv( __DIR__ . "/.." );
 $dotenv->load();
 
 use PHPUnit\Framework\TestCase;
 
-class LightrailTest extends TestCase
-{
-	public function testEnvVarsSet()
-	{
-		$this->assertNotEmpty(getEnv("LIGHTRAIL_API_KEY"));
-		$this->assertNotEmpty(getEnv("LIGHTRAIL_SHARED_SECRET"));
-		$this->assertNotEmpty(getEnv("CONTACT_ID"));
-		$this->assertNotEmpty(getEnv("SHOPPER_ID"));
+class LightrailTest extends TestCase {
+	public function testEnvVarsSet() {
+		$this->assertNotEmpty( getEnv( "LIGHTRAIL_API_KEY" ) );
+		$this->assertNotEmpty( getEnv( "LIGHTRAIL_SHARED_SECRET" ) );
+		$this->assertNotEmpty( getEnv( "CONTACT_ID" ) );
+		$this->assertNotEmpty( getEnv( "SHOPPER_ID" ) );
 	}
 
-	public function testPing()
-    {
-        Lightrail::$apiKey = getEnv("LIGHTRAIL_API_KEY");
-        $response = LightrailAPICall::ping();
-        $this->assertEquals('TEST', $response['user']['mode']);
-    }
+	public function testPing() {
+		Lightrail::$apiKey = getEnv( "LIGHTRAIL_API_KEY" );
+		$response          = LightrailAPICall::ping();
+		$this->assertEquals( 'TEST', $response['user']['mode'] );
+	}
 }
