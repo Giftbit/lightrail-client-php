9c205abc7097aa7dcfedda1d24170b37f4ae11fb
Tana Jukes <tana.j@lightrail.com>
2017-11-21 17:35:11 -0800

    Whitespace fix (contact test)

--- tests/LightrailContactTest.php
+++ tests/LightrailContactTest.php
@@ -3,26 +3,24 @@
 namespace Lightrail;
 require_once __DIR__ . '/../init.php';
 
-$dotenv = new \Dotenv\Dotenv(__DIR__ . "/..");
+$dotenv = new \Dotenv\Dotenv( __DIR__ . "/.." );
 $dotenv->load();
 
 use PHPUnit\Framework\TestCase;
 
-class LightrailContactTest extends TestCase
-{
+class LightrailContactTest extends TestCase {
 
-    public function testRetrieve()
-    {
-        Lightrail::$apiKey = getEnv("LIGHTRAIL_API_KEY");
-        $contactById = LightrailContact::retrieveByContactId(getEnv("CONTACT_ID"));
-        $contactByShopperId = LightrailContact::retrieveByShopperId(getEnv("SHOPPER_ID"));
-        $this->assertEquals(getEnv("CONTACT_ID"), $contactByShopperId->contactId);
-        $this->assertEquals(getEnv("SHOPPER_ID"), $contactById->userSuppliedId);
+	public function testRetrieve() {
+		Lightrail::$apiKey  = getEnv( "LIGHTRAIL_API_KEY" );
+		$contactById        = LightrailContact::retrieveByContactId( getEnv( "CONTACT_ID" ) );
+		$contactByShopperId = LightrailContact::retrieveByShopperId( getEnv( "SHOPPER_ID" ) );
+		$this->assertEquals( getEnv( "CONTACT_ID" ), $contactByShopperId->contactId );
+		$this->assertEquals( getEnv( "SHOPPER_ID" ), $contactById->userSuppliedId );
 
-        $cardFromContactId = $contactById->retrieveContactCardForCurrency('USD');
-        $cardFromShopperId = $contactByShopperId->retrieveContactCardForCurrency('USD');
+		$cardFromContactId = $contactById->retrieveContactCardForCurrency( 'USD' );
+		$cardFromShopperId = $contactByShopperId->retrieveContactCardForCurrency( 'USD' );
 
-        $this->assertEquals($cardFromContactId->cardId, $cardFromShopperId->cardId);
-    }
+		$this->assertEquals( $cardFromContactId->cardId, $cardFromShopperId->cardId );
+	}
 
 }
