4d26f1114cdd18c165a66d4043a96799acb195cc
jeff <jeff@giftbit.com>
2018-03-09 14:56:54 -0800

    Add metadata to shopper token.

--- lib/LightrailShopperTokenFactory.php
+++ lib/LightrailShopperTokenFactory.php
@@ -4,12 +4,25 @@ namespace Lightrail;
 
 class LightrailShopperTokenFactory
 {
-    public static function generate($contact, $validityInSeconds = 43200)
+    /**
+     * Generate a shopper token that can be used to make Lightrail calls
+     * restricted to that particular shopper.  The shopper can be defined by the
+     * contactId, userSuppliedId, or shopperId.
+     *
+     * eg: `generateShopperToken(array("shopperId" => "user-12345"));`
+     * eg: `generateShopperToken(array("shopperId" => "user-12345"), array("validityInSeconds" => 43200, "metadata" => array("foo" => "bar")));`
+     *
+     * @param $contact array an associative array that defines one of: contactId, userSuppliedId or shopperId
+     * @param $options array an associative array that may define: `validityInSeconds` the number of seconds the shopper token will be valid for,
+     *              `metadata` additional data that can be signed in the shopper token.
+     * @return string the shopper token
+     */
+    public static function generate($contact, $options = array())
     {
-        if ( ! isset(Lightrail::$apiKey) || empty(Lightrail::$apiKey)) {
+        if (!isset(Lightrail::$apiKey) || empty(Lightrail::$apiKey)) {
             throw new Exceptions\BadParameterException("Lightrail.apiKey is empty or not set.");
         }
-        if ( ! isset(Lightrail::$sharedSecret) || empty(Lightrail::$sharedSecret)) {
+        if (!isset(Lightrail::$sharedSecret) || empty(Lightrail::$sharedSecret)) {
             throw new Exceptions\BadParameterException('Lightrail.sharedSecret is not set.');
         }
 
@@ -23,6 +36,24 @@ class LightrailShopperTokenFactory
             throw new Exceptions\BadParameterException("contact must set one of: shopperId, contactId, userSuppliedId");
         }
 
+        $validityInSeconds = 43200;
+        $metadata = null;
+        if (is_numeric($options)) {
+            // Support for legacy code when the second param was validityInSeconds.
+            $validityInSeconds = $options;
+        } elseif (is_array($options)) {
+            if (isset($options['validityInSeconds'])) {
+                $validityInSeconds = $options['validityInSeconds'];
+            }
+            if (isset($options['metadata'])) {
+                $metadata = $options['metadata'];
+            }
+        }
+
+        if ($validityInSeconds <= 0) {
+            throw new Exceptions\BadParameterException("validityInSeconds must be > 0");
+        }
+
         $payload = explode('.', Lightrail::$apiKey);
         $payload = json_decode(base64_decode($payload[1]), true);
 
@@ -33,12 +64,12 @@ class LightrailShopperTokenFactory
                     'gmi' => $payload['g']['gmi']
                 ) + $g,
             'iat' => $iat,
-            'iss' => "MERCHANT"
+            'exp' =>  $iat + $validityInSeconds,
+            'iss' => "MERCHANT",
+            'roles' => ['shopper']
         );
-
-        if (isset($validityInSeconds)) {
-            $exp = $iat + $validityInSeconds;
-            $token['exp'] = $exp;
+        if (!is_null($metadata)) {
+            $token['metadata'] = $metadata;
         }
 
         $jwt = \Firebase\JWT\JWT::encode($token, Lightrail::$sharedSecret, 'HS256');
